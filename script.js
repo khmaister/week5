a2af0ab70ffea8ae70fb05a3e2172f2c0f07b1ad
van khin <khmaister@yahoo.com>
2023-09-25 16:40:22 -0400

    recall timeslot description upon page reload

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
   // Code to display the current date in the header of the page.
   var today = dayjs();
   $("#currentDay").text(today.format('dddd, MMM D YYYY, h:mm a'));
 
 
-  // Listener for save button to log.
+  // Listener for save button to log input.
   var eventText;
   var eventTime;
 
@@ -17,10 +13,7 @@ $(function () {
     console.log(eventText);
     eventTime = $(this).siblings(".hour").text();
     console.log(eventTime);
-    localStorage.setItem(eventTime, JSON.stringify(eventText));
-
-    
-    
+    localStorage.setItem(eventTime, JSON.stringify(eventText));       
   });
 
   // Code to apply the past, present, or future class to each time
@@ -45,10 +38,46 @@ $(function () {
         $(this).addClass("present");
       }
   });
-  //
+  
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+  var saveEventText9 = JSON.parse(localStorage.getItem("9AM"));
+  $("#9").val("");
+  $("#9").val(saveEventText9);
+  
+  var saveEventText10 = JSON.parse(localStorage.getItem("10AM"));
+  $("#10").val("");
+  $("#10").val(saveEventText10);
+  
+  var saveEventText11 = JSON.parse(localStorage.getItem("11AM"));
+  $("#11").val("");
+  $("#11").val(saveEventText11);
+  
+  var saveEventText12 = JSON.parse(localStorage.getItem("12PM"));
+  $("#12").val("");
+  $("#12").val(saveEventText12);
+  
+  var saveEventText1 = JSON.parse(localStorage.getItem("1PM"));
+  $("#13").val("");
+  $("#13").val(saveEventText1);
+
+  var saveEventText2 = JSON.parse(localStorage.getItem("2PM"));
+  $("#14").val("");
+  $("#14").val(saveEventText2);
+
+  var saveEventText3 = JSON.parse(localStorage.getItem("3PM"));
+  $("#15").val("");
+  $("#15").val(saveEventText3);
+
+  var saveEventText4 = JSON.parse(localStorage.getItem("4PM"));
+  $("#16").val("");
+  $("#16").val(saveEventText4);
+
+  var saveEventText5 = JSON.parse(localStorage.getItem("5PM"));
+  $("#17").val("");
+  $("#17").val(saveEventText5);
   
 });
